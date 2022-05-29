219c2a78780b8dbb7eb96f04c4097ee9cc2d1272
RASIDUL <rasidulhaque017@gmail.com>
2022-05-29 18:16:24 +0600

    first commit

---
+++ resources/views/account/navigation_bottom.blade.php
@@ -0,0 +1,5 @@
+<div class="footer">
+	<div class="container">
+		<b class="copyright">&copy; {{ date('Y') }} - HAQUE Library Management System design by MD. RASIDUL HAQUE</b> 
+	</div>
+</div>
---
+++ resources/views/layout/template_footer.blade.php
@@ -0,0 +1,5 @@
+;<div class="footer" style="background-color: #00cc00;">
+	<div class="container">
+		<center><b class="copyright" style="color: white;">&copy; {{ date('Y') }} - HAQUE Library Management System design by MD. RASIDUL HAQUE. </b> </center>
+	</div>
+</div>
